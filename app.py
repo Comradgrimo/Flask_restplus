28ced20ddb550de05dd8dc410784d74833ae7816
Grimo <comradgrimo@gmail.com>
2019-03-10 21:30:27 +0300

    add new

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app, title='Сотрудники')
+api = api.namespace('todos', description='TODO operations')
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///people.db'
@@ -55,39 +56,79 @@ a_people = api.model('People', {'fio': fields.String('The language.'),
                                 'birthday': fields.String('The birthday.'),
                                 'office': fields.String('The office.')
                                   })
-
-PEOPLE = [
-    {"id": "1", "fio": "Иванов И.И", "birthday": "11.01.11", "office": "Инженер"},
-    {"id": "2", "fio": "Петров П.П", "birthday": "12.02.12", "office": "Уборщик"},
-    {"id": "3", "fio": "Сидоров С.С", "birthday": "13.03.13", "office": "Главбух"},
- ]
-#PEOPLE = {'language' : 'aaaaa', 'id' : '1'}
+#
+# PEOPLE = [
+#     {"id": "1", "fio": "Иванов И.И", "birthday": "11.01.11", "office": "Инженер"},
+#     {"id": "2", "fio": "Петров П.П", "birthday": "12.02.12", "office": "Уборщик"},
+#     {"id": "3", "fio": "Сидоров С.С", "birthday": "13.03.13", "office": "Главбух"},
+#  ]
+# #PEOPLE = {'language' : 'aaaaa', 'id' : '1'}
 
 @api.route('/people')
 class People(Resource):
     def get(self):
+        '''Показать все'''
         all_users = Person.query.order_by(Person.person_id).all()
         result = users_schema.dump(all_users)
-        return jsonify(result.data)
+        return result
     @api.expect(a_people)
-    def post(self):
+    def put(self):
+        '''Добавить человека'''
         new_people = api.payload
-        #new_people['person_id'] = (len(Person.query.all())) + 1
-        #new_people['timestamp'] = datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S")
-        db.session.add(Person(fio = 'bbbbb', birthday ='bbbbb', office ='bbbbbb'))
+        db.session.add(Person(fio = new_people['fio'], birthday =new_people['birthday'], office =new_people['office']))
         db.session.commit()
         return new_people
 #
 @api.route('/people/<id>')
 class People1(Resource):
     def get(self, id):
-        person = Person.query.filter(Person.person_id == id).all()
-        result = users_schema.dump(person, id)
+        '''Выбрать человека по его ID'''
+        person = Person.query.filter(Person.person_id == id).one_or_none()
+        if person is not None:
+            result = users_schema.dump(person, id)
+            return jsonify(result.data)
+        else:
+            return {'Ненайден сотрудник с id №': id}, 404
+    def delete(self, id):
+        '''Удалить запись'''
+        person = Person.query.filter(Person.person_id == id).one_or_none()
+        if person is not None:
+            db.session.delete(person)
+            db.session.commit()
+            return {'deleted person id №': id}, 201
+        else:
+            return {'Ненайден сотрудник с id №': id}, 404
+    @api.expect(a_people)
+    def post(self,id):
+        '''Изменить запись'''
+        new_people = api.payload
+        person = Person.query.filter(Person.person_id == id).one_or_none()
+        if person is not None:
+            person.fio = new_people['fio']
+            person.birthday = new_people['birthday']
+            person.office = new_people['office']
+            db.session.commit()
+            return {'update №': person.fio}, 201
+        else:
+            return {'Ненайден сотрудник с id №': id}, 404
+
+@api.route('/people/office/<office>')
+class People2(Resource):
+    def get(self, office):
+        '''Выбрать человека по его Должности'''
+        person = Person.query.filter(Person.office == office).all()
+        #if person is not None:
+        result = users_schema.dump(person, office)
         return jsonify(result.data)
-#
-#     def get(self, id):
-#         res = [x for x in PEOPLE if x['id'] == id]
-#         return {'result': res}, 201
+        #else:
+           # return {'Ненайден сотрудник с длжностью %s:': office }, 404
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+#
+# def filter():
+#     olderThan = request.args.get('olderThan', None)
+#     yongerThan = request.args.get('yongerTnah', None)
+
